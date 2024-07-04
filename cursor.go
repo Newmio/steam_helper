e4afb51b31a8fe183167ea4e0c73d6fea724969f
newmio <newm.dev@gmail.com>
2024-07-04 20:48:03 +0300

    struct funcs

--- cursor.go
+++ cursor.go
@@ -1,6 +1,33 @@
 package steam_helper
 
-import "context"
+import (
+	"context"
+	"encoding/json"
+)
+
+func MapToStruct(m map[string]interface{}, s interface{})error{
+	data, err := json.Marshal(m)
+	if err != nil{
+		return err
+	}
+
+	return json.Unmarshal(data, &s)
+}
+
+func StructToMap(s interface{})(map[string]interface{}, error){
+	data, err := json.Marshal(s)
+	if err != nil{
+		return nil, err
+	}
+
+	var m map[string]interface{}
+
+	if err := json.Unmarshal(data, &m); err != nil{
+		return nil, err
+	}
+
+	return m, nil
+}
 
 type (
 	Cursor[T any] struct {
